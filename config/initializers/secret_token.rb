bee88f1d763bb2ed044c775e0641bca479c498e1
VLP <nittin4664@gmail.com>
2013-07-15 22:16:20 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VLPBill::Application.config.secret_token = '12e34bbd5b5d090a882c44b9fc3c3284d690842ea72fbf0b0d5c1e3ce4963aec082c166fbcbff950a6b02f919a6903b555d39852bb960dcfd0e3958450217d1a'
